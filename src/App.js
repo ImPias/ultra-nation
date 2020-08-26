4b4431d900e39f316162c45f030ebb609d295f67
impias <kazipias123@gmail.com>
2020-08-27 00:12:51 +0600

    Ultra-Nation final after Module-34.5

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Country from './Components/Country/Country';
+import Cart from './Components/Cart/Cart';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(res => res.json())
+    .then(data => {
+      setCountries(data);
+      // console.log(data);
+      // const name = data.map(country => country.name);
+      // console.log(name);
+    })
+    .catch(error => console.log(error))
+  }, []);
+
+  const [cart, setCart] = useState([])
+  const handleAddCountry = (country) => {
+    const newCart = [...cart, country];
+    setCart(newCart);
+  };
+
   return (
     <div className="App">
+      <h1>Countries Loaded: {countries.length}</h1>
+      <h4>Added Country: {cart.length}</h4>
+      <Cart cart={cart}></Cart>
+      <ul>
+        {
+          countries.map(country => <Country country={country} handleAddCountry={handleAddCountry} key={country.alpha3Code}></Country>)
+        }
+      </ul>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
